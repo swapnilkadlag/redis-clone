8dec551bb52d6d48210fce64b1d6469955df51c5
Swapnil Kadlag <swapnil.kadlag.27@gmail.com>
2022-03-16 01:23:27 +0530

    Initial database implementation

---
+++ app/database.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+var db = struct {
+	sync.RWMutex
+	data map[any]any
+}{data: make(map[any]any)}
+
+func getKey(key any) any {
+	db.RLock()
+	value := db.data[key]
+	db.RUnlock()
+	return value
+}
+
+func setKey(key any, value any, px int) {
+	db.Lock()
+	db.data[key] = value
+	db.Unlock()
+	if px > 0 {
+		go expireKey(key, px)
+	}
+}
+
+func expireKey(key any, ms int) {
+	var duration = time.Duration(ms * int(time.Millisecond))
+	time.Sleep(duration)
+	deleteKey(key)
+}
+
+func deleteKey(key any) {
+	db.Lock()
+	delete(db.data, key)
+	db.Unlock()
+}
